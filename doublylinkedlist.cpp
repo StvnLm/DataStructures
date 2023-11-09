4c534063871ff1d4e17730a5ba790d0032bdcfaa
Steven Lam <34800247+StvnLm@users.noreply.github.com>
2023-11-08 22:46:54 -0500

    Update doublylinkedlist.cpp

--- doublylinkedlist.cpp
+++ doublylinkedlist.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
@@ -15,36 +7,57 @@ typedef struct Node {
     Node* prev;
 }* nodePtr;
 
+
+void printForward(Node*& head) {
+    nodePtr current = head;
+    std::cout << "nullptr -> ";
+    while (current != nullptr) {
+        std::cout << current->data << " -> ";
+        current = current->next;
+    }
+    std::cout << "nullptr" << std::endl;
+}
+
+
+void printBackward(Node*& tail) {
+
+}
+
 int main()
 {
     
+    nodePtr node;
     nodePtr head;
     nodePtr tail;
-    nodePtr curr;
-    nodePtr tmp;
+    node = new Node;
     
-    Node* node = new Node;
-    
-    node->data = 1;
+    node->data = 4;
     node->next = nullptr;
     node->prev = nullptr;
+    
     head = node;
     tail = node;
     
-    Node* node2 = new Node;
-    node->data = 2;
+    node = new Node;
+    node->data = 5;
     node->next = nullptr;
-    node->prev = tail; 
-    tail->next = node2;
-    head = node2;
+    node->prev = tail;
+    tail->next = node;
+    tail = node;
+
+    node = new Node;
+    node->data = 6;
+    node->next = nullptr;
+    node->prev = tail;
+    tail->next = node;
+    tail = node;
+
+    node = new Node;
+    node->data = 7;
+    node->next = nullptr;
+    node->prev = tail;
+    tail->next = node;
+    tail = node;
     
-    tmp = tail;
-    
-    while (tmp != nullptr) {
-        std::cout << tmp->data << std::endl;
-        tmp = tmp->next;
-    }
-    
-    
-    return 0;
+    printForward(head);
 }
