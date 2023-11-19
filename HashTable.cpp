91187552b1d7c904cca96c1181ce633fd08cf174
Steven Lam <34800247+StvnLm@users.noreply.github.com>
2023-11-18 21:28:34 -0500

    add some more functions

--- HashTable.cpp
+++ HashTable.cpp
@@ -73,9 +73,99 @@ public:
     }
 
     void printTable()
-    {
-    
+    {   item* ptr;
+        for (int i = 0; i < tableSize; i++)
+        {
+            cout << "------------------------" << endl;
+            cout << "Index = " << i << endl;
+            ptr = HashTable[i];
+            while (ptr != nullptr && HashTable[i]->name != "empty")
+            {
+                cout << ptr->name << endl;
+                cout << ptr->drink << endl;
+                ptr = ptr->next;
+            }
+        }
     };
+
+    void findDrink(string name)
+    {
+        int index = hash(name);
+        item* ptr;
+        bool foundName = false;
+        string drink;
+        ptr = HashTable[index];
+        
+        while (ptr != nullptr)
+        {
+            if (ptr->name == name)
+            {
+                foundName = true;
+                drink = ptr->drink;
+            }
+            ptr = ptr->next;
+        }
+        if (foundName == true)
+        {
+            cout << name << "'s favourite drink is " << drink << endl;
+        }
+        else
+        {
+            cout << name << "'s info was not found in the hash table" << endl;
+        }
+    }
+
+    void removeItem(string name)
+    {
+        item* delPtr;
+        item* p1;
+        item* p2;
+        int index = hash(name);
+        
+        if (HashTable[index]->name == "empty")
+        {
+            cout << name << " was not found in the hash table" << endl;
+        }
+        
+        if (HashTable[index]->name == name && HashTable[index]->next == nullptr)
+        {
+            HashTable[index]->name = "empty";
+            HashTable[index]->drink = "empty";
+            HashTable[index]->next = nullptr;
+            cout << name << " has been deleted from the hash table" << endl;
+        }
+        else if  (HashTable[index]->name == name && HashTable[index]->next != nullptr)
+        {
+            delPtr = HashTable[index];
+            HashTable[index] = HashTable[index]->next;
+            delete delPtr;
+            cout << name << " has been deleted from the hash table" << endl;
+        }
+        else
+        {
+            p1 = HashTable[index]->next;
+            p2 = HashTable[index];
+            
+            while (p1 != nullptr && p1->name != name)
+            {
+                p2 = p1;
+                p1 = p1->next;
+            }
+            
+            if (p1 == nullptr)
+            {
+                cout << name << " was not found in the hash table" << endl;
+            }
+            else 
+            {
+                delPtr = p1;
+                p1 = p1->next;
+                p2->next = p1;
+                delete delPtr;
+                cout << name << " has been deleted from the hash table" << endl;
+            }
+        }
+    }
 
 private:
     static const int tableSize = 10;
@@ -94,14 +184,12 @@ int main()
 {
     Hash myHash; // Create an instance of the Hash class
     myHash.addItem("Steven", "Milk Tea");
-    std::cout << "Steven index = " << myHash.hash("Steven") << std::endl;
     myHash.addItem("Linda", "Water");
-    std::cout << "Linda index = " << myHash.hash("Linda") << std::endl;
     myHash.addItem("Luna", "Water");
-    std::cout << "Luna index = " << myHash.hash("Luna") << std::endl;
     myHash.addItem("Miso", "Orange Juice");
-    std::cout << "Miso index = " << myHash.hash("Miso") << std::endl;
-    std::cout << "Num of items in index = " << myHash.numberOfItemsInIndex(8) << std::endl;
+    myHash.findDrink("Steven");
+    myHash.removeItem("Linda");
+    myHash.printTable();
     
     return 0;
 }
