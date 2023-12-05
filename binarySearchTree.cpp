c59d75b02b293af3ab616cba83c318ec102265ac
Steven Lam <34800247+StvnLm@users.noreply.github.com>
2023-12-04 19:54:44 -0500

    Update binarySearchTree.cpp

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
@@ -64,25 +56,90 @@ class BinarySearchTree {
                 return searchRecursive(rootPtr->rightPtr, key);
             }
         }
+        
+
+        
+        int findMin(){
+            if (rootPtr == nullptr){
+                std::cout << "Tree is empty" << std::endl;
+                return 0;
+            }
+
+            // Traverse to the left-most Node
+            Node* cursor = rootPtr;
+            while (cursor->leftPtr != nullptr){
+                cursor = cursor->leftPtr;
+            }
+            
+            return cursor->key;
+            
+        };
+        
+        int findMax(){
+            if (rootPtr == nullptr){
+                std::cout << "Tree is empty" << std::endl;
+                return 0;
+            }
+
+            // Traverse to the right-most Node
+            Node* cursor = rootPtr;
+            while (cursor->rightPtr != nullptr){
+                cursor = cursor->rightPtr;
+            }
+            
+            return cursor->key;
+            
+        };
+        
+        // void remove(int key){};
+        
+        int getHeightOfTree() {
+            return getHeight(rootPtr); // Call getHeight starting from the root
+        }
+        
+        int getHeight(Node* nodePtr){
+            if (nodePtr == nullptr){
+                return 0;
+            }
+            else {
+                int leftHeight = getHeight(nodePtr->leftPtr);
+                int rightHeight = getHeight(nodePtr->rightPtr);
+                return 1 + std::max(leftHeight, rightHeight);
+            }
+        };
+        
+        // void inOrderTraversal(){};
+        
+        // void preOrderTraversal(){};
+        
+        // void postORderTraversal(){};
+        
 };
 
 int main()
 {
     BinarySearchTree tree;
-    tree.insert(1);
-    tree.insert(2);
+    tree.insert(20);
+    tree.insert(10);
+    tree.insert(50);
+    tree.insert(60);
     tree.insert(5);
-    tree.insert(9);
-    
-    for (int i = 0; i < 10; i++) {
-        bool found = tree.search(i);
-        if (found) {
-            std::cout << "Found " << i << " in the tree." << std::endl;
-        }
-        else {
-            std::cout << "Not found " << i << " in the tree." << std::endl;
-        }
-    }
+    tree.insert(1);
+    tree.insert(4);
+    tree.insert(2);
+    tree.insert(3);
+    // std::cout << tree.findMax() << std::endl;
+    // std::cout << tree.findMin() << std::endl;
+    std::cout << tree.getHeightOfTree() << std::endl;
+    // for (int i = 0; i < 10; i++) {
+    //     bool found = tree.search(i);
+    //     if (found) {
+    //         std::cout << "Found " << i << " in the tree." << std::endl;
+    //     }
+    //     else {
+    //         std::cout << "Not found " << i << " in the tree." << std::endl;
+    //     }
+    // }
 
     return 0;
 }
